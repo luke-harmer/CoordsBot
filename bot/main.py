0b14ee6a5966530e78a3ab1b25ac3b2f501f8e13
MMCrypto <DurinsBane@MinesofMoriaCrypto.farm>
2021-12-29 20:22:48 -0600

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,9 @@
 import os
+import re
 
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 import discord
 from discord import Color
 from discord.ext import commands
